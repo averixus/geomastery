5f0099d167e7b94792108a2261a6ab83a641e70f
Jay Avery <a.j.rowe@outlook.com>
2017-07-10 18:37:56 +0100

    Make flat roofs drip in rain

--- src/java/jayavery/geomastery/blocks/BlockFlatroof.java
+++ src/java/jayavery/geomastery/blocks/BlockFlatroof.java
@@ -7,6 +7,7 @@
 package jayavery.geomastery.blocks;
 
 import java.util.List;
+import java.util.Random;
 import com.google.common.collect.Lists;
 import jayavery.geomastery.utilities.BlockMaterial;
 import jayavery.geomastery.utilities.BlockWeight;
@@ -20,10 +21,13 @@ import net.minecraft.entity.passive.EntityAmbientCreature;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /** Flat breakable roof block. */
 public class BlockFlatroof extends BlockBuilding {
@@ -118,6 +122,24 @@ public class BlockFlatroof extends BlockBuilding {
         boolean isSupported = BlockWeight.getWeight(world.getBlockState(pos
                 .down()).getBlock()).canSupport(this.getWeight());
         return isRoof && isSupported;
+    }
+    
+    /** Make drips when raining (as vanilla leaves). */
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void randomDisplayTick(IBlockState stateIn, World world,
+            BlockPos pos, Random rand) {
+        
+        if (world.isRainingAt(pos.up()) && !world.getBlockState(pos.down())
+                .isSideSolid(world, pos.down(), EnumFacing.UP) &&
+                rand.nextInt(15) == 1) {
+            
+            double d0 = pos.getX() + rand.nextFloat();
+            double d1 = pos.getY() - 0.05D;
+            double d2 = pos.getZ() + rand.nextFloat();
+            world.spawnParticle(EnumParticleTypes.DRIP_WATER, d0, d1, d2,
+                    0.0D, 0.0D, 0.0D);
+        }
     }
     
     @Override
