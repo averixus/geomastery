d62a6f891ed85474d40883320ae7ef51ba472710
Jay Avery <a.j.rowe@outlook.com>
2017-07-12 18:28:02 +0100

    Forge update for @Config GUI

--- src/java/jayavery/geomastery/main/GeoConfig.java
+++ src/java/jayavery/geomastery/main/GeoConfig.java
@@ -6,35 +6,30 @@
  ******************************************************************************/
 package jayavery.geomastery.main;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import com.google.common.collect.Lists;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Config.Comment;
-import net.minecraftforge.common.config.ConfigElement;
-import net.minecraftforge.common.config.Property;
-import net.minecraftforge.fml.client.IModGuiFactory;
-import net.minecraftforge.fml.client.config.GuiConfig;
-import net.minecraftforge.fml.client.config.IConfigElement;
+import net.minecraftforge.common.config.Config.LangKey;
 
 @Config(modid = Geomastery.MODID)
 public class GeoConfig {
 
     @Comment("Show the food type in the item tooltip")
+    @LangKey("geomastery:config.foodTooltips")
     public static boolean foodTooltips = true;
 
     @Comment("Show the item's decay level with the filled proportion of durability bar as well as colour")
+    @LangKey("geomastery:config.foodDurability")
     public static boolean foodDurability = false;
 
     @Comment("Show the biomes where a crop can grow in the item tooltip")
+    @LangKey("geomastery:config.cropTooltips")
     public static boolean cropTooltips = true;
 
     @Comment("Hide unusued and inaccessible vanilla items from Just Enough Items")
+    @LangKey("geomastery:config.hideVanilla")
     public static boolean hideVanilla = true;
     
-    @Comment("Show simple building block requirements in the item tooltip")
+    @Comment("Show building block requirements in the item tooltip")
+    @LangKey("geomastery:config.buildTooltips")
     public static boolean buildTooltips = true;
 }
