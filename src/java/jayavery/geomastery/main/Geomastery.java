990626ccab7dfad5ec93d773aa422b9309089268
Jay Avery <a.j.rowe@outlook.com>
2017-07-26 09:44:58 +0100

    2.2.1

--- src/java/jayavery/geomastery/main/Geomastery.java
+++ src/java/jayavery/geomastery/main/Geomastery.java
@@ -95,7 +95,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class Geomastery {
 
     public static final String MODID = "geomastery";
-    public static final String VERSION = "2.2.0";
+    public static final String VERSION = "2.2.1";
     public static final String NAME = "Geomastery";
     public static final String MC_VER = "1.11.2-13.20.1.2386";
     public static final String UPDATE = "https://gist.githubusercontent.com/JayAvery/97013d9f3a4d3dd904fb608899d9eadd/raw/";
